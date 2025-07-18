cac01cdd79e2fbc872c440eeaf7a3cf59dd9ee56
GDubuisson <dubuisson.guillaume@gmail.com>
2025-07-18 17:32:56 +0200

    feat: WIP

--- README.md
+++
@@ -1,8 +0,0 @@
-# TRPG_TW_FR
-Tabletop RPG - The Witcher FR
-
-## Introduction
-
-Je voulais jouer au TRPG The witcher sur FoundryVTT.
-Manque de bol : pas de traduction Française, et des BDD verrouillées.
-Donc, je le refait moi même.
--- module/setup/registerSheets.js
+++
@@ -1,9 +0,0 @@
-import {TWActorSheet} from "../actors/TWActorSheet.js";
-
-export const registerSheets = () => {
-    Items.unregisterSheet('core', ActorSheet);
-    Items.registerSheet('witcher', TWActorSheet, {
-        makeDefault: true,
-        types: ['personnage']
-    });
-}
